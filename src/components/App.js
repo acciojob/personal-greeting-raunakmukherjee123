7650e4ca2f77505b99f221ae26b518afd08e6fa0
Raunak Mukherjee <153613701+raunakmukherjee123@users.noreply.github.com>
2025-06-29 13:43:52 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,48 @@
 
-import React from "react";
+// import React from "react";
+// import './../styles/App.css';
+
+// const App = () => {
+//   const [input,setInput]=useState('');
+//   const onAdd=(e)=>{
+//     const value=e.target.value;
+//     setInput(value);
+//   }
+//   const onClear=()=>{
+//     setInput('');
+//   }
+//   return (
+//     <div>
+//        <p>Enter your name:</p>
+//         <input value={input} onChange={onAdd} />
+//        {!!input.length && <p>Hello {input}!</p>}
+//         <button onClick={onClear}>Clear</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+  import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
-  const [input,setInput]=useState('');
-  const onAdd=(e)=>{
-    const value=e.target.value;
-    setInput(value);
+  const [input,setInput] = useState('');
+  const onInput = (e)=>{
+      const {value} = e.target
+    setInput(value)
   }
-  const onClear=()=>{
-    setInput('');
+  const onClear = ()=>{
+    setInput('')
   }
+  
   return (
     <div>
-       <p>Enter your name:</p>
-        <input value={input} onChange={onAdd} />
-       {!!input.length && <p>Hello {input}!</p>}
-        <button onClick={onClear}>Clear</button>
+      <p>Enter your name:</p>
+    <input value={input} onChange={onInput} />
+    {!!input.length&&<p>Hello {input}!</p>}
+    <button onClick={onClear}>Clear</button>
     </div>
   )
 }
